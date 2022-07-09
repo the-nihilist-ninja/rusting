6b4381287a70481531a945f79ff60aea8ec693be
the-nihilist-ninja <richydany@gmail.com>
2022-07-09 15:55:42 +0200

    Using buffer reader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
 
 /// Creating a grep tool
 #[derive(Parser, Debug)]
@@ -11,17 +14,28 @@ struct Cli{
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Cli::parse();
     println!("{:?}",args);
 
-    let content = std::fs::read_to_string(&args.path).expect("Could not read file!");
+    // let content = std::fs::read_to_string(&args.path).expect("Could not read file!");
+    
+    let f = File::open(&args.path)?;
+    let reader = BufReader::new(f);
 
-    for line in content.lines(){
-        if line.contains(&args.pattern){
-            println!("{}", line);
+    for line in reader.lines() {
+        let l = line?;
+        if l.contains(&args.pattern){
+            println!("{}", l);
         }
     }
 
 
+    // for line in content.lines(){
+    //    if line.contains(&args.pattern){
+    //        println!("{}", line);
+    //    }
+    // }
+
+    Ok(())
 }
