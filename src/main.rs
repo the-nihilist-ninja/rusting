839be8aaa7e8ff4e978592e4070e0a707f74146b
the-nihilist-ninja <richydany@gmail.com>
2022-07-09 15:23:16 +0200

    Added first cut implementation of grep tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::Parser;
+
+/// Creating a grep tool
+#[derive(Parser, Debug)]
+struct Cli{
+    /// Pattern to look for
+    pattern: String,
+
+    /// Path to read file from
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("{:?}",args);
+
+    let content = std::fs::read_to_string(&args.path).expect("Could not read file!");
+
+    for line in content.lines(){
+        if line.contains(&args.pattern){
+            println!("{}", line);
+        }
+    }
+
+
 }
