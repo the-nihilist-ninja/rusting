487ecccd271cd8b20055faef6debc35de0703dd2
the-nihilist-ninja <richydany@gmail.com>
2022-07-23 16:42:19 +0200

    Checked reading records

--- src/loaders/record.rs
+++ src/loaders/record.rs
@@ -1,11 +1,11 @@
 use std::error::Error;
 
 use serde::{Serialize, Deserialize};
-use chrono::{DateTime, Utc};
+use chrono::NaiveDate;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Record {
-    adult: Option<bool>,
+pub struct Metadata {
+    adult: Option<String>,
     belongs_to_collection:Option<String>,
     budget: f32,
     genres: Option<String>,
@@ -20,26 +20,28 @@ pub struct Record {
     production_companies: Option<String>,
     production_countries: Option<String>,
     #[serde(with = "my_date_format")]
-    release_date: DateTime<Utc>,
-    revenue:f32,
-    runtime:f32,
+    release_date: NaiveDate,
+    // release_date: Option<String>,
+    revenue:Option<f32>,
+    runtime:Option<f32>,
     spoken_languages: String,
     status: Option<String>,
     tagline: Option<String>,
     title: String,
-    video: Option<bool>,
+    video: Option<String>,
     vote_average: Option<f32>,
     vote_count: Option<u64>,
 }
 
+// Took from official documentation
 mod my_date_format{
-    use chrono::{DateTime, Utc, TimeZone};
+    use chrono::{Date, NaiveDate};
     use serde::{self, Deserialize, Serializer, Deserializer};
 
     const FORMAT: &'static str = "%Y-%m-%d";
 
     pub fn serialize<S>(
-        date: &DateTime<Utc>,
+        date: &NaiveDate,
         serializer: S,
     ) -> Result<S::Ok, S::Error>
     where
@@ -49,31 +51,24 @@ mod my_date_format{
         serializer.serialize_str(&s)
     }
 
-    // The signature of a deserialize_with function must follow the pattern:
-    //
-    //    fn deserialize<'de, D>(D) -> Result<T, D::Error>
-    //    where
-    //        D: Deserializer<'de>
-    //
-    // although it may also be generic over the output types T.
     pub fn deserialize<'de, D>(
         deserializer: D,
-    ) -> Result<DateTime<Utc>, D::Error>
+    ) -> Result<NaiveDate, D::Error>
     where
         D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
-        Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+        NaiveDate::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
     }
 }
 
-fn example(record_path:String) -> Result<(), Box<dyn Error>> {
+pub fn read_record(record_path:String) -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_path(record_path)?;
     for result in rdr.deserialize() {
         // Notice that we need to provide a type hint for automatic
-        // deserialization.
-        let record: Record = result?;
-        println!("{:?}", record);
+        // deserialization.    
+        let rc: Metadata = result?;
+    // sprintln!("{:?}", rc);
     }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
 mod loaders;
 
-use clap::Parser;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::fs::File;
-use anyhow::{Context,  Result};
+use loaders::record::*;
 
-use crate::loaders::record::*;
+fn main(){
 
-/// Creating a grep tool
-#[derive(Parser)]
-struct Cli{
-    /// Pattern to look for
-    pattern: String,
 
-    /// Path to read file from
-    #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
-
-fn main() -> Result<()> {
-    let args = Cli::parse();
-    
-    let f = File::open(&args.path).with_context(|| format!("Unable to locate file: {}", &args.path.into_os_string().into_string().unwrap()))?;
-    let reader = BufReader::new(f);
-
-    for line in reader.lines() {
-        let l = line?;
-        if l.contains(&args.pattern){
-            println!("{}", l);
-        }
+    if let Err(err) = read_record("data/movies_metadata.csv".to_string()) {
+        println!("{}", err);
+        std::process::exit(1);
     }
 
-    Ok(())
 }
